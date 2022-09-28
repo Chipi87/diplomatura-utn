69ea720fdb3e6c47c9a36aa38ae5918c5c961ba5
Chipi87 <mgautp@outlook.es>
2022-09-27 22:09:06 -0300

    clase

---
+++ clase10js/JS/main.js
@@ -0,0 +1,28 @@
+
+/*
+console.log('hola mundo');
+
+let nombre= "Marcos"
+console.log(nombre)
+
+let apellido="Gauto"
+console.log(apellido)
+
+let edad=35
+console.log(edad)
+
+let mascota=false
+console.log(mascota)
+*/
+
+// sin comillas para que muestre lo que guarde en la variable //
+
+
+
+
+
+/* PRONT */
+
+let entrada = prompt("Ingresar nombre");
+let salida =  entrada+ " " + ","+" "+"Muchas gracias"
+alert(salida)
