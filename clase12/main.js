3ff64e14f1fbf0c5f67e03ae1de401afc831eb7a
Chipi87 <mgauto@outlook.es>
2022-10-08 01:36:02 -0300

    actividades

---
+++ clase12/main.js
@@ -0,0 +1,20 @@
+/** bucles e interacciones */
+
+//while
+
+///incremento
+/**
+let i=0;
+while (i < 55){
+    console.log("el valor de i: " ,i);
+    i=i+1;
+}
+
+
+let i=100;
+
+while(i>0){
+    document.write("solo faltan ", i , " pasos por caminar.",'<br>');
+    i=i-1;
+}
+ */
---
+++ clase12mati/main.js
@@ -0,0 +1,34 @@
+/*
+//ejecicio 1 clase 10 ( Variables en la consola)
+let Nombre="Marcos"
+console.log(Nombre)
+let Edad=17
+console.log(Edad)
+let mascota = "true"
+console.log(mascota)
+
+//ejecicio 2 clase 10
+(Prompt y alerta con if / else para definir si es menor o mayor de edad)
+
+let edad=prompt("Que edad tienes?")
+if( edad<= 18 ){
+    alert("es menor de edad");
+}else(edad>18);{
+    alert("Es mayor de edad");
+}
+
+
+
+const superMercado = ["yerba","azucar","fideos","cafe","arroz","manteca","leche"];
+
+let articulo=prompt("que articulo desea agregar?")
+
+if ( superMercado.includes(articulo)){
+    alert(articulo+" ya se encuentra");
+} else (
+    superMercado.push(articulo));{
+        alert("La lista es "+superMercado)
+    }
+
+*/
+
