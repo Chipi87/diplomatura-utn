d30c9dfd83c29dba32562fbf783182f2275bde4e
Chipi87 <mgauto@outlook.es>
2022-10-18 22:09:35 -0300

    clase15

--- Clase 13/main.js
+++ Clase 13/main.js
@@ -15,6 +15,26 @@ return nombre.length;
 let completo = dato("Marcos")
 console.log( "Resultado " + completo)
 
+
+
+//Pedir dos numeros al usuarioy sumarlos
+
+function sumar(a, b) {
+    return a + b
+
+}
+
+let sumar = (a, b) => a + b
+
+const result = (parseFloat(prompt("Ingrese un numero")), parseFloat(prompt("Ingrese otro numero")))
+
+
+console.log(result);
+
 */
 
+let sumar = (a, b) => a + b
 
+let result=sumar (parseFloat(prompt("ingresar primer numero")), parseFloat(prompt("Ingrese segundo numero")))
+
+console.log(result);
---
+++ Clase 14/main.js
@@ -0,0 +1,11 @@
+/* Objetos */
+
+//Declaracion de un objeto literal
+
+const producto1 = {
+    id: 1,
+    nombre: "Tallarines",
+    precio: 80.5
+}
+
+console.log();
