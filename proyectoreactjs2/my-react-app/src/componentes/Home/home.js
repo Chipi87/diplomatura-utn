c9cdc03a2d6f488f3c500c2b77ff5810600ef2fa
Chipi87 <mgauto@outlook.es>
2022-11-10 22:13:56 -0300

    ReactJs

---
+++ proyectoreactjs2/my-react-app/src/componentes/Home/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./home.css";
+
+const Home = () =>{
+    return(
+        <header>
+            <h1> Soy header</h1>
+        </header>
+    );}
+
+export default Home;
